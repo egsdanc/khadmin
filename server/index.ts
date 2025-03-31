f9e3d17b55bb807337c5d71a747f52c0312c7792
orhan4a <orhan@4alabs.io>
2025-03-31 04:52:15 +0300

    fixupload

--- server/index.ts
+++ server/index.ts
@@ -30,8 +30,9 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
-    app.use(express.static(path.join(__dirname, 'public')));
+ //  app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+ app.use('/uploads', express.static('uploads'));  
+ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
