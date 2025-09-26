7ec9fa8e24a1755f856c512ed50b5004d7e1edf8
orhan4a <orhan@4alabs.io>
2025-09-26 13:51:01 +0300

    qf

--- server/index.ts
+++ server/index.ts
@@ -32,6 +32,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+//
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+//
 // Alternatif olarak, mevcut public klasörünü de serve edebilirsiniz
 app.use('/public', express.static(path.join(process.cwd(), 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
